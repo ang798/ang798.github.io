c7279ac57e2b7800755e66d2aee346dcb1d412ef
qatou11 <109033556+qatou11@users.noreply.github.com>
2024-05-19 19:34:16 +0300

    Create bdconnect.php

---
+++ bdconnect.php
@@ -0,0 +1,10 @@
+<?php
+    $servername = "217.71.129.139";
+    $username = "root";
+    $password = "Mpk234a0672ccz"; 
+    $db = "osamu";
+    $conn = mysqli_connect($servername, $username, $password, $db);
+
+    $sql = "SELECT * FROM videos"
+    $result = mysqli_query($conn, $sql);
+?>
